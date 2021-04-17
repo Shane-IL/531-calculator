3eb3964d336763f6ec61f32671dbf5cee0ecc421
Shane <shanegoodson101@gmail.com>
2021-04-17 21:16:26 +0300

    MAde title generates, values save to local storage

--- index.js
+++ index.js
@@ -9,10 +9,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const calcTable = document.querySelector('.calc-table');
 
+    const titlesHTML = `
+    <div class="title placeholder"></div>
+    <div class="title title-1rm">1 RM</div>
+    <div class="title title-base">90% Base</div>
+    <div class="title title-95">95% of base</div>
+    <div class="title title-90">90% of base</div>
+    <div class="title title-85">85% of base</div>
+    <div class="title title-80">80% of base</div>
+    <div class="title title-75">75% of base</div>
+    <div class="title title-70">70% of base</div>
+    <div class="title title-65">65% of base</div>
+    <div class="title title-60">60% of base</div>
+    <div class="title title-50">50% of base</div>
+    <div class="title title-40">40% of base</div>`
+
     const calculateAndAppend = (table, value, title, classIdentifier) => {
         const base = value*0.9;
         const titleEl = document.createElement('div');
-        titleEl.className = `exercise-title ${classIdentifier}-title`;
+        titleEl.className = `title exercise-title ${classIdentifier}-title`;
         titleEl.innerText = title;
         table.appendChild(titleEl);
 
@@ -23,14 +38,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const baseEl = document.createElement('div');
         baseEl.className = `value ${classIdentifier}-base`;
-        baseEl.innerText = base;
+        baseEl.innerText = round(base);
         table.appendChild(baseEl);
 
         calcValues.forEach(calcValue => {
             const printValue = Math.round(calcValue * 100);
             const valueEl = document.createElement('div');
             valueEl.className = `value ${classIdentifier}-${printValue}`;
-            valueEl.innerText = round((base*calcValue), 2).toFixed(2);
+            valueEl.innerText = round(base*calcValue, 2);
             table.appendChild(valueEl);
         });
     }
@@ -40,6 +55,11 @@ document.addEventListener('DOMContentLoaded', () => {
         return Number(num + "e" + -decimalPlaces);
     }
 
+    spInput.value = localStorage.getItem('sp1RM') || null;
+    dlInput.value = localStorage.getItem('dl1RM') || null;
+    bpInput.value = localStorage.getItem('bp1RM') || null;
+    sqInput.value = localStorage.getItem('sq1RM') || null;
+
     btnCalc.addEventListener('click', e => {
         const sp1RM = spInput.value;
         const dl1RM = dlInput.value;
@@ -47,6 +67,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const sq1RM = sqInput.value;
 
         if(sp1RM && dl1RM && bp1RM && sq1RM) {
+            calcTable.innerHTML = titlesHTML;
+
+            localStorage.setItem('sp1RM', sp1RM);
+            localStorage.setItem('dl1RM', dl1RM);
+            localStorage.setItem('bp1RM', bp1RM);
+            localStorage.setItem('sq1RM', sq1RM);
+
             calculateAndAppend(calcTable, sp1RM, "Standing Shoulder Press", "sp");
             calculateAndAppend(calcTable, dl1RM, "Deadlift", "dl");
             calculateAndAppend(calcTable, bp1RM, "Bench Press", "bp");
